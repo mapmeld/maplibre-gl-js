1f533bf1c2a9c87e54ec409a05ecc404ec4ba2aa
Nick Doiron <ndoiron@mapmeld.com>
2025-08-09 20:28:18 -0500

    separate out carat

--- src/symbol/transform_text.ts
+++ src/symbol/transform_text.ts
@@ -6,9 +6,16 @@ import {type Formatted} from '@maplibre/maplibre-gl-style-spec';
 
 /* Kurdish ligature constants */
 const connectors = '[\u0628\u062A\u062B\u062C\u062D\u062E\u0633\u0634\u0635\u0636\u0637\u0638\u0639\u063A\u0641\u0642\u0643\u0644\u0645\u0646\u0647\u064A\u06A9\u06B5]';
+/*
+  candidates
+  https://www.compart.com/en/unicode/U+030C
+  https://www.compart.com/en/unicode/U+036E
+*/
+const carat = '\u036E';
 const connectorRegEx = new RegExp(`(?<=${connectors})ڵا`, 'g');
-const isolate = '̌ﻻ';
-const final = '̌ﻼ';
+const isolate = `${carat}ﻻ`;
+const finalPreset = new RegExp(isolate, 'g');
+const final = `${carat}ﻼ`;
 
 function transformTextInternal(text: string, layer: SymbolStyleLayer, feature: Feature) {
     const transform = layer.layout.get('text-transform').evaluate(feature, {});
@@ -19,7 +26,7 @@ function transformTextInternal(text: string, layer: SymbolStyleLayer, feature: F
     }
 
     if (rtlWorkerPlugin.applyArabicShaping) {
-        text = rtlWorkerPlugin.applyArabicShaping(text.replace(connectorRegEx, final)).replace(/̌ﻻ/g, final).replace(/ڵﺎ/g, isolate);
+        text = rtlWorkerPlugin.applyArabicShaping(text.replace(connectorRegEx, final)).replace(finalPreset, final).replace(/ڵﺎ/g, isolate);
     }
 
     return text;
